fcb8967610e1334457857953a1d2569e78186783
Tiago Knoch <tiagoknoch@gmail.com>
2023-12-13 13:25:36 +0100

    Day05 part 1

--- Day05.cs
+++ Day05.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using Spectre.Console;
 
@@ -44,7 +45,40 @@ public class Day05 : BaseDay
         return new(results.Min().ToString());
     }
 
-    private List<List<RangeRecord>> ParseMaps(IEnumerable<string> lines)
+
+    public override ValueTask<string> Solve_2()
+    {
+        var lines = _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var seeds = ParseSeeds2(lines.First());
+        var maps = ParseMaps(lines.Skip(1));
+        var results = new List<long>();
+
+        foreach (var seedRange in seeds)
+        {
+            foreach (var seed in EnumerableRangeLong(seedRange.Item1, seedRange.Item2))
+            {
+                long currentLocationValue = seed;
+                foreach (var map in maps)
+                {
+                    var rangeRecord = map.FirstOrDefault(item => currentLocationValue >= item.Source && currentLocationValue < item.Source + item.Range);
+
+                    if (rangeRecord == null)
+                    {
+                        continue;
+                    }
+
+                    var difference = currentLocationValue - rangeRecord.Source;
+                    currentLocationValue = rangeRecord.Destination + difference;
+                }
+
+                results.Add(currentLocationValue);
+            }
+        }
+
+        return new(results.Min().ToString());
+    }
+
+    private static List<List<RangeRecord>> ParseMaps(IEnumerable<string> lines)
     {
         var pattern = @"(.+)-to-(.+) map:";
         var maps = new List<List<RangeRecord>>();
@@ -85,8 +119,36 @@ public class Day05 : BaseDay
         .ToArray();
     }
 
-    public override ValueTask<string> Solve_2()
+    private static IEnumerable<(long, long)> ParseSeeds2(string line)
     {
-        return new(0.ToString());
+        var numbers = line.Split(':')[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(long.Parse)
+        .ToArray();
+
+        var arrPairs = Split(numbers, 2);
+
+        foreach (var item in arrPairs)
+        {
+            yield return (item[0], item[1]);
+        }
     }
+
+    private static IEnumerable<long> EnumerableRangeLong(long start, long count)
+    {
+        var end = start + count;
+        for (var current = start; current < end; ++current)
+        {
+            yield return current;
+        }
+    }
+
+    private static List<List<T>> Split<T>(IList<T> source, int length)
+    {
+        return Enumerable
+            .Range(0, (source.Count + length - 1) / length)
+            .Select(n => source.Skip(n * length).Take(length).ToList())
+            .ToList();
+    }
+
 }
---
+++ Day06.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private readonly string _input;
+
+    public Day06()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var lines = _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var pairs = Parse(lines);
+
+        var result = pairs
+            .Select(ProcessRace)
+            .Aggregate((a, x) => a * x);
+
+        return new(result.ToString());
+    }
+
+    private int ProcessRace((int Time, int Record) race)
+    {
+        var (time, record) = race;
+        var waysToWin = Enumerable.Range(1, time - 2)
+                                  .Count(i => (time - i) * i > record);
+
+        return waysToWin;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static List<(int, int)> Parse(string[] lines)
+    {
+        var times = lines[0]
+            .Split(':')[1]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+        var distances = lines[1]
+            .Split(':')[1]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        return times.Zip(distances, (time, distance) => (time, distance)).ToList();
+    }
+}
